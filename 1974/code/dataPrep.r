c1e8ec44a9574dbb432a326591b6e9bee14739ca
emagar <emagar@gmail.com>
2017-02-09 09:51:06 -0600

    Arian: 1) prepara codebook, 2) revisa ioth

--- 1974/code/dataPrep.r
+++ 1974/code/dataPrep.r
@@ -1,8 +1,7 @@
-# ARIAN
-# Fuente: http://www.politicsresources.net/
+# Fuente de los datos: http://www.politicsresources.net/
 
 rm(list = ls())
-setwd("/home/eric/Dropbox/data/elecs/uk/1974/data/") # ESTO LO TIENES QUE ADAPTAR A TU MAQUINA
+setwd("/home/eric/Dropbox/data/elecs/uk/1974/data/")
 #setwd("/home/lobo/Github/uk/1974/data/")
 
 # lee datos brutos
@@ -45,7 +44,6 @@ procesa <- function(X = NULL){
     X$sh2 <- X$v2/X$vtot
     X$sh3 <- X$v3/X$vtot
     X$shoth <- X$voth/X$vtot
-    
     X <- X[duplicated(X$n)==FALSE,]
     X <- X[,c("n","constituency","region","norirl","status","electorate","turnout","ncand","vtot","v1","v2","v3","voth","sh1","sh2","sh3","shoth","c1","c2","c3","p1","p2","p3")]
     return(X)
@@ -56,18 +54,8 @@ feb <- procesa(feb)
 # prepara datos de la eleccion de octubre
 oct <- procesa(oct)
 
-head(feb)
-
-incumbent <- function(X=NULL, incu=NULL,colaño=NULL){
-  X$i1<-as.numeric(pmatch(X$c1,incu[,colaño], nomatch=0)>0)
-  X$i2<-as.numeric(pmatch(X$c2,incu[,colaño], nomatch=0)>0)
-  X$i3<-as.numeric(pmatch(X$c3,incu[,colaño], nomatch=0)>0)
-  X$ioth<-as.numeric( !(X$i1+X$i2+X$i3)>0)
-  return(X)
-}
 # ARIAN
 # FALTA CODIFICAR CUATRO DUMMIES
-
 # feb$i1   = 1 si c1 es incumbent (si ganó el distrito en t-1),   = 0 de otro modo
 #feb$i1<-as.numeric(pmatch(feb$c1,incumb[,1], nomatch=0)>0)
 # feb$i2   = 1 si c2 es incumbent (si ganó el distrito en t-1),   = 0 de otro modo
@@ -76,15 +64,23 @@ incumbent <- function(X=NULL, incu=NULL,colaño=NULL){
 #feb$i3<-as.numeric(pmatch(feb$c3,incumb[,1], nomatch=0)>0)
 # feb$ioth = 1 si otro es incumbent (si ganó el distrito en t-1), = 0 de otro modo
 #feb$ioth<-as.numeric( !(feb$i1+feb$i2+feb$i3)>0)
+incumbent <- function(X=NULL, incu=NULL,colaño=NULL){
+  X$i1<-as.numeric(pmatch(X$c1,incu[,colaño], nomatch=0)>0)
+  X$i2<-as.numeric(pmatch(X$c2,incu[,colaño], nomatch=0)>0)
+  X$i3<-as.numeric(pmatch(X$c3,incu[,colaño], nomatch=0)>0)
+  #X$ioth<-as.numeric( !(X$i1+X$i2+X$i3)>0) <- ARIAN: ESTO ESTA MAL, DE ESTE MODO ES COMPLEMENTO... HAY QUE BUSCAR DESDE BASE febRaw SI ALGUN NOMBRE COINCIDE.
+  return(X)
+}
 
 feb<-incumbent(feb,incumb,1)
 oct<-incumbent(oct,incumb,2)
 
-# IDEM PARA oct
-# PREPARA TAMBIEN codebook.txt 
+head(feb)
 
 write.csv(feb, file = "uk1974feb.csv", row.names = FALSE)
 write.csv(oct, file = "uk1974oct.csv", row.names = FALSE)
+
+# ARIAN: PREPARA codebook.txt que describa las variables de los datos anteriores 
 
 
 
